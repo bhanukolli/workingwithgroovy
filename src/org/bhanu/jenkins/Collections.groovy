f8b7d164394a29ff17ad6bf3ea47efa4a95440a2
Kolli, Bhanu <bhkolli@stubhub.com>
2017-10-09 14:09:59 -0700

    Updated Collections.groovy.

--- src/org/bhanu/jenkins/Collections.groovy
+++ src/org/bhanu/jenkins/Collections.groovy
@@ -1,6 +1,6 @@
 def beateles = ["John", "Paul" , "George"]
 
-for (int i =0 ; i< beateles.size(); i++){
+for (def i =0 ; i< beateles.size; i++){
     def greeting = "Hello, "
     println "$greeting" + "${beateles[i]}"
     println "${i*10}"
